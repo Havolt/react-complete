80aee4872bed4da626f184b0ea2808458c8198e8
Mark <markfitz815@gmail.com>
2024-01-27 19:15:41 +0000

    Obtain investmentData from util function

--- 05-essential-project/src/App.jsx
+++ 05-essential-project/src/App.jsx
@@ -2,43 +2,44 @@ import InvHeader from './components/InvHeader'
 import InvForm from './components/InvForm'
 import InvResult from './components/InvResult'
 
-import { useState } from 'react';
+import { useState } from 'react'
+import { calculateInvestmentResults } from './util/investment'
 
 function App() {
 
-  const [investmentInitial, setInvestmentInitial] = useState(0);
-  const [investmentAnnual, setInvestmentAnnual] = useState(0);
-  const [investmentReturn, setInvestmentReturn] = useState(0);
-  const [investmentDuration, setInvestmentDuration] = useState(0);
+  const [initialInvestment, setInitialInvestment] = useState(0);
+  const [annualInvestment, setAnnualInvestment] = useState(0);
+  const [expectedReturn, setExpectedReturn] = useState(0);
+  const [duration, setDuration] = useState(0);
 
-  const updateInvestmentInitial = (newValue) => {
-    console.log('Initial')
-    setInvestmentInitial(newValue)
+  const investmentData = calculateInvestmentResults({initialInvestment, annualInvestment, expectedReturn, duration})
+
+  console.log(investmentData)
+
+  const updateinitialInvestment = (newValue) => {
+    setInitialInvestment(newValue)
   }
 
-  const updateInvestmentAnnual = (newValue) => {
-    console.log('Annual')
-    setInvestmentAnnual(newValue)
+  const updateAnnualInvestment = (newValue) => {
+    setAnnualInvestment(newValue)
   }
 
-  const updateInvestmentReturn = (newValue) => {
-    console.log('Return')
-    setInvestmentReturn(newValue)
+  const updateExpectedReturn = (newValue) => {
+    setExpectedReturn(newValue)
   }
 
-  const updateInvestmentDuration = (newValue) => {
-    console.log('Duration')
-    setInvestmentDuration(newValue)
+  const updateDuration = (newValue) => {
+    setDuration(newValue)
   }
 
   return (
     <div className="app">
       <InvHeader />
       <InvForm 
-        updateInitial={updateInvestmentInitial}
-        updateAnnual={updateInvestmentAnnual}
-        updateReturn={updateInvestmentReturn}
-        updateDuration={updateInvestmentDuration}
+        updateInitial={updateinitialInvestment}
+        updateAnnual={updateAnnualInvestment}
+        updateReturn={updateExpectedReturn}
+        updateDuration={updateDuration}
       />
       <InvResult />
     </div>
--- 05-essential-project/src/components/InvInput.jsx
+++ 05-essential-project/src/components/InvInput.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function InvInput({inputType = 'number', inputName, inputLabel, onUpdate}) {
 
   const handleUpdate = (event) => {
-    console.log(event)
     const inputVal = event.target.value
     onUpdate(+inputVal)
   }
