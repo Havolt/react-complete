293ea0f44c979a6b3f13d0c2f8631fc8e96cb3ce
Mark <markfitz815@gmail.com>
2024-02-07 21:01:39 +0000

    Add content to default page

--- 09-project-management-app/src/components/ProjectDefault.jsx
+++ 09-project-management-app/src/components/ProjectDefault.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import NoProjectsImg from "../assets/no-projects.png"
 
 function ProjectDefault() {
   return (
-    <div>ProjectDefault</div>
+    <div className="flex flex-col items-center gap-4">
+      <img src={NoProjectsImg} alt="" className="w-16" />
+      <h1 className="font-bold">No project selected</h1>
+      <p>Select a project or get started with a new one</p>
+      <button className="bg-black rounded-md text-white p-1">Create new project</button>
+    </div>
   )
 }
 
