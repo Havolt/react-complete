2c024bebbfcff5f0b3bbdf54afe0853781ee7ddf
Mark <markfitz815@gmail.com>
2024-02-17 20:10:04 +0000

    Add functions for starting quiz and moving to next questions

--- 13-react-quiz/src/components/Quiz.jsx
+++ 13-react-quiz/src/components/Quiz.jsx
@@ -6,11 +6,24 @@ import QUESTIONS from '../util/questions'
 function Quiz() {
 
   const [currentQuestion, setCurrentQuestion] = useState(null)
+  const [quizOver, setQuizOver] = useState(false)
+
+  const startQuiz = () => {
+    setCurrentQuestion(0);
+  }
+
+  const moveToNextQuestion = () => {
+    setCurrentQuestion(prevState => {
+      return prevState + 1 < QUESTIONS.length ? prevState + 1 : prevState;
+    })
+  }
 
   return (
     <div>
-      { currentQuestion !== null && <Question currentQ={QUESTIONS[currentQuestion]} /> }
-      <Summary />
+      { currentQuestion !== null ? 
+        <Question currentQ={QUESTIONS[currentQuestion]} /> : 
+        <button onClick={startQuiz}>Start Quiz</button> }
+      { quizOver && <Summary /> }
     </div>
   )
 }
